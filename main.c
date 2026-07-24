7c52a15ef5eb767fdf047759826be147b79dc945
John Masi <johnrmasi@gmail.com>
2026-07-23 20:35:38 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
-#include <avr/interrupt.h> 
-#include <string.h>
-#include <util/atomic.h>
-#include "uart.h"
+#include "driver.h"
 
-// Our reponse from RX line that goes to the microcontroller tx line 
-// and the computer gets it back on the rx line 
+ISR(ADC_vect) {
+  value = ADC;
+}
+
+ISR(TIMER1_COMPA_vect) {
+  count++;
+  flag = 1;
+}
+
 ISR(USART0_UDRE_vect) {
   UDR0 = tx_buffer[tx_tail++];
   if(tx_tail >= sizeof(tx_buffer)) {
@@ -19,7 +23,6 @@ ISR(USART0_UDRE_vect) {
 }
 
 ISR(USART0_RX_vect){
-  // Reiceiving bytes from the computer TX Line 
   char c = UDR0;
 
   if(c == '\b' || c == 0x7F) {
@@ -42,43 +45,43 @@ ISR(USART0_RX_vect){
       x_char(c);
   }
 
-  PORTB ^= (1 << 7);
-
 }
 
 int main() {
   DDRB |= (1 << 7);
+  adc_init();
   timer_init();
   usart_init();
   sei();
 
   while(1) {
-    poll_timer();
     if(ready) {
-      ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-        ready = 0;
-      }
+      ready = 0;
       if(strcmp(buffer,"On") == 0) {
-        PORTB |= (1 << 7);
+        //PORTB |= (1 << 7);
         tx_string("LED ON\n\r");
       }
       else if(strcmp(buffer,"Off") == 0) {
-        PORTB &= ~(1 << 7);
+        //PORTB &= ~(1 << 7);
       }
       else if(strcmp(buffer, "--v") == 0) {
-        tx_string("GPIO UART v.1.0\r\n");
+        tx_string("GPIO UART v.1.1\r\n");
       }
       else if(strcmp(buffer, "uptime") == 0) {
-        itoa(count,buffer,10);
-        tx_string(buffer);
-        x_char('\n');
-        x_char('\r');
+        //itoa(count,buffer,10);
+        //tx_string(buffer);
+        //x_char('\n');
+        //x_char('\r');
       }
-      ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      else if(strcmp(buffer, "volt") == 0) {
+          //adc_poll();
+          TCCR1B |= (1 << CS02) | (1 << CS00);
+      }
+     
         head = 0;
         buffer[0] = '\0';
-      }
     }
+    adc_poll();
     
   }
 }
